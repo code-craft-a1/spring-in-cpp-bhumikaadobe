c205f24942ab26b6fa4c1e3e54b83862fc724968
bhumikaadobe <bhumi.umar@gmail.com>
2025-04-24 16:32:11 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -6,8 +6,8 @@ namespace Statistics {
         double average;
         double max;
         double min;
-        Stats(double avg = 0.0, double minV = 0.0, double maxV = 0.0)
-		   : average(avg), min(minV), max(maxV) {}
+        Stats(double avg = std::numeric_limits<double>::quiet_NaN(), double minV = std::numeric_limits<double>::quiet_NaN(), double maxV = std::numeric_limits<double>::quiet_NaN())
+               : average(avg), min(minV), max(maxV) {}
     };
     Stats ComputeStatistics(const std::vector<double>& input);
 }
