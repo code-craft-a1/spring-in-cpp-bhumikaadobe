99c2ca7b49cfb7643f7b5dd79cc1248e8a1fc88e
bhumikaadobe <bhumi.umar@gmail.com>
2025-04-24 16:22:45 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -6,6 +6,8 @@ namespace Statistics {
         double average;
         double max;
         double min;
+        Stats(double avg = 0.0, double minV = 0.0, double maxV = 0.0)
+		   : average(avg), min(minV), max(maxV) {}
     };
     Stats ComputeStatistics(const std::vector<double>& input);
 }
