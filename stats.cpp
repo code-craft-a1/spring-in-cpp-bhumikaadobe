90e5a350ad0836054f7c9f4e5bfaeef2ab71f299
bhumikaadobe <bhumi.umar@gmail.com>
2025-04-24 21:58:59 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,16 +2,18 @@
 #include <numeric>
 #include <algorithm>
 
-Stats Statistics::ComputeStatistics(const std::vector<double>& input) {
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<double>& input) {
     if (input.empty())
     {
         double notNumber = std::numeric_limits<double>::quiet_NaN();
         return Statistics::Stats(notNumber,notNumber,notNumber);
     }
+    
     Statistics::Stats stats;
     double sum = std::accumulate(input.begin(), input.end(), 0.0);
     stats.average = sum / input.size();
     stats.max = *std::max_element(input.begin(), input.end());
     stats.min = *std::min_element(input.begin(), input.end());
 
+     return stats;
 }
